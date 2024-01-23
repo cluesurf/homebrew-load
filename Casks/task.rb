cf6cc100bba68647a3ad4f8d933060d6edd39810
Lance Pollard <lp@elk.fm>
2024-01-23 06:15:35 -0800

    better python

--- Casks/task.rb
+++ Casks/task.rb
@@ -3,7 +3,7 @@ cask "task" do
   desc "Common Actions Interface Dependencies"
   homepage "https://github.com/termsurf/task"
   url "https://github.com/termsurf/homebrew-load/raw/make/Tool/task.zip"
-  version "0.0.34"
+  version "0.0.44"
   sha256 "c065d769b89fe209152a3c64034bd46355c01868c0ebbbb06173870d8324a649"
 
   depends_on macos: ">= :sierra"
@@ -36,14 +36,20 @@ cask "task" do
   depends_on formula: "swift-format"
   depends_on cask: "julia"
   depends_on formula: "php"
+  depends_on formula: "antlr"
+  depends_on formula: "black"
 
   installer script: "install.sh"
 
   postflight do
+    # https://stackoverflow.com/questions/67074684/pip-has-problems-with-metadata
+    system_command "find", args: ["#{HOMEBREW_PREFIX}/lib/python3.11/site-packages", "-empty", "-type", "d", "-delete"]
     system_command "#{HOMEBREW_PREFIX}/bin/go", args: ["install", "github.com/klauspost/asmfmt/cmd/asmfmt@ef134b9cec704e2b7b336fb02153b7d1a58247da"]
     system_command "#{HOMEBREW_PREFIX}/bin/go", args: ["install", "mvdan.cc/sh/v3/cmd/shfmt@v3.7.0"]
-    system_command "pip3", args: ["install", "nbconvert"]
-    system_command "pip3", args: ["install", "docx2pdf"]
+    system_command "#{HOMEBREW_PREFIX}/bin/pip3", args: ["install", "--upgrade", "--no-cache-dir", "--use-deprecated=legacy-resolver", "nbconvert"]
+    system_command "#{HOMEBREW_PREFIX}/bin/pip3", args: ["install", "--upgrade", "--no-cache-dir", "--use-deprecated=legacy-resolver", "docx2pdf"]
+    system_command "#{HOMEBREW_PREFIX}/bin/pip3", args: ["install", "--upgrade", "--no-cache-dir", "--use-deprecated=legacy-resolver", "black[jupyter]"]
+    system_command "#{HOMEBREW_PREFIX}/bin/pip3", args: ["install", "--upgrade", "--no-cache-dir", "--use-deprecated=legacy-resolver", "patool"]
   end
 
   def caveats
